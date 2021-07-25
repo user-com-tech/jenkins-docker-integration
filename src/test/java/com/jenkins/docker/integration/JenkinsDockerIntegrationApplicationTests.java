e254b57c9c1a2cbc584ce5a2b81d0145706302d2
Vivek Kumar Mishra <vivek.mishra1@comviva.com>
2021-07-26 00:47:28 +0530

    initial commit

---
+++ src/test/java/com/jenkins/docker/integration/JenkinsDockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.docker.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
